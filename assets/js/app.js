3b48f5630b0f65053a44299e312b2c78c2dc8a62
Carlos cruz valencia <a2carcruzinfo@gmail.com>
2023-01-27 19:05:04 +0100

    Initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+let Piramid = new Zdog.Illustration({
+    element: '.zdog-canvas',
+    centered: true,
+    resize: 'fullscreen',
+    backface: "#636",
+    dragRotate: true,
+});
+
+
+
+illo.updateRenderGraph();
